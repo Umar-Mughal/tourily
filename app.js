1265b39192719971dc097908ecfa4c9b0e8d98b2
Umar Farooq <umar.wzbd@gmail.com>
2022-11-17 23:08:57 +0500

    Delete Tour

--- app.js
+++ app.js
@@ -71,8 +71,25 @@ app.patch('/api/v1/tours/:id', (req, res) => {
         }
     })
 })
-const port = 3000;
 
+// Delete Tour
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
+// Run Server
+const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port} ...`)
 })
