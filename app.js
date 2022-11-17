43d577d375eac9e0e906ec8bc5c5f56a9d98e7d4
Umar Farooq <umar.wzbd@gmail.com>
2022-11-17 23:04:58 +0500

    Update Tour

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/test-data/data/tour-simple.json`))
 
+// Create Tour
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body)
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/test-data/data/tour-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    })
+})
+
+// Get All Tours
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -16,6 +33,7 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+// Get Tour
 app.get('/api/v1/tours/:id', (req, res) => {
     const tourId = req.params.id * 1
     const tour = tours.find(el => el.id === tourId);
@@ -35,21 +53,24 @@ app.get('/api/v1/tours/:id', (req, res) => {
     });
 });
 
-app.post('/api/v1/tours', (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body)
-    tours.push(newTour)
 
-    fs.writeFile(`${__dirname}/test-data/data/tour-simple.json`, JSON.stringify(tours), (err) => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-               tour: newTour
-            }
-        });
+// Update Tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+    if(req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '(Updated Tour Here)'
+        }
     })
 })
-
 const port = 3000;
 
 app.listen(port, () => {
