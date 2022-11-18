8862f9a961a0bb928e6f856e975ce11b1328c2c9
Umar Farooq <umar.wzbd@gmail.com>
2022-11-18 17:51:14 +0500

    Data Security by Environment Variables & dotenv

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const userRouter = require('./routes/user.routes');
 const app = express();
 
 // Middlewares
+if(process.env.NODE_ENV === 'dev') {
+    app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use(morgan('dev'));
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// Packages
+const dotenv = require('dotenv').config({path: `${__dirname}/env/.env.${process.env.NODE_ENV}`})
 // App
 const app = require('./app')
 
+console.log(process.env)
+
 // Start Server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port} ...`)
 })
