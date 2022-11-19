fc70414804169db91e55f77e3c02079b1149da87
Umar Farooq <umar.wzbd@gmail.com>
2022-11-19 12:04:25 +0500

    Create a Simple Tour Model

--- configs/db.js
+++ configs/db.js
@@ -11,3 +11,21 @@ mongoose
   .connect(Db)
   .then(() => console.log(`DB connection successful!!!`))
   .catch((err) => console.log(`DB connection failed!!!`));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
