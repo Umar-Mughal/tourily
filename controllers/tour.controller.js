7331677918863885fc6bc016b8d492a8a40b7f40
Umar Farooq <umar.wzbd@gmail.com>
2022-11-19 18:59:10 +0500

    Deleting Documents

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -74,11 +74,19 @@ const updateTour = async (req, res) => {
   }
 };
 
-const deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+const deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(204).json({
+      status: 'success',
+      data: err,
+    });
+  }
 };
 
 module.exports = {
