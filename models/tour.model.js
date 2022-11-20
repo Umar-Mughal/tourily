15d3ce958b96a0b787ae4b0fb00de971a3cd3171
Umar Farooq <umar.wzbd@gmail.com>
2022-11-20 19:04:26 +0500

    Extended the Tour Model

--- models/tour.model.js
+++ models/tour.model.js
@@ -6,16 +6,50 @@ const tourSchema = mongoose.Schema({
     required: [true, 'A tour must have a name.'],
     unique: [true, 'Tour name should be unique.'],
   },
-  duration: Number,
-  difficulty: String,
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty.'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price'],
+    required: [true, 'A tour must have a price.'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary.'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image.'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
