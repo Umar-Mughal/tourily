3064ca1e5d63acc8743e2f54a263be535021bc4c
Umar Farooq <umar.wzbd@gmail.com>
2022-12-30 17:56:22 +0500

    Query & Aggregate Middlewares

--- http/base/APIFeatures.js
+++ http/base/APIFeatures.js
@@ -36,7 +36,7 @@ class APIFeatures {
 
   paginate() {
     const page = this.reqQuery.page * 1 || 1;
-    const limit = this.reqQuery.limit * 1 || 10;
+    const limit = this.reqQuery.limit * 1 || 100;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     return this;
--- models/tour/tourModel.js
+++ models/tour/tourModel.js
@@ -54,6 +54,10 @@ const tourSchema = mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -61,19 +65,39 @@ const tourSchema = mongoose.Schema(
   }
 );
 
-/** VIRTUAL PROPERTIES **/
+//------ VIRTUAL PROPERTIES (These properties are not part of the actual DB schema) ------//
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-/** DOCUMENT MIDDLEWARES **/
-//Runs before .save() and .create(), but not for .insertMany()
+//------ DOCUMENT MIDDLEWARES ------//
+// Runs before .save() and .create(), but not for .insertMany()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
-//Runs After .save() and .create()
+// Runs After .save() and .create()
 tourSchema.post('save', (doc, next) => {
+  // this is just a regular function, linter made it an arrow one, please change it to regular function to get "this"
+  next();
+});
+
+//------ QUERY MIDDLEWARES (This keyword now will point current query not document) ------//
+// This /^find/ RE will trigger this function for all the queries contain word "find".
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds.`);
+  console.log(docs);
+  next();
+});
+
+//------ Aggregation MIDDLEWARES (Here "this" will point to an aggregation object) ------//
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
 
