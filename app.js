3e456105c63c2200e79a7b248a537dc320ac9b2f
Umar Farooq <umar.wzbd@gmail.com>
2023-01-04 12:57:15 +0500

    Handling unhandled routes

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const morgan = require('morgan');
 // Routers
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+// Controllers
+const notFoundController = require('./http/controllers/notFound/notFoundController');
 
 const app = express();
 
-// Middlewares
+//---- Middlewares ----//
 if (process.env.NODE_ENV === 'dev') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// Routes
+//---- Routes ----//
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', notFoundController.notFound);
 
 module.exports = app;
