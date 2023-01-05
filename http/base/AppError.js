b6f406e98831554778f3fed297fbef17419dd09a
Umar Farooq <umar.wzbd@gmail.com>
2023-01-05 17:27:40 +0500

    save

--- http/base/AppError.js
+++ http/base/AppError.js
@@ -3,7 +3,7 @@ class AppError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.isOperationl = true;
+    this.isOperationl = true; // an indicator that error is created by ourselves
     Error.captureStackTrace(this, this.constructor);
   }
 }
