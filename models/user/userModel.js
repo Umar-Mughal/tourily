6e1729b8410070a69f5e4127f0755ef0e53c7ac1
Umar Farooq <umar.wzbd@gmail.com>
2023-01-26 07:42:21 +0500

    Added User Model

---
+++ models/user/userModel.js
@@ -0,0 +1,27 @@
+// Packages
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell use your email'],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
